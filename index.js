a7ef2426681b4e8cbea95d27be89c317890ef571
Shailendra_Singh <singhshailendra1701@gmail.com>
2022-07-06 21:53:52 +0530

    web-App

---
+++ index.js
@@ -0,0 +1,9 @@
+var card = document.getElementById("card");
+function openregister() {
+  card.style.transform = "rotateY(-180deg)";
+}
+function openlogin() {
+  card.style.transform = "rotateY(0deg)";
+}
+
+console.log(firebase);
